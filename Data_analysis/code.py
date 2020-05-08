2eec0e2387e500b8ab213984a8588f3753091eee
AmrutaPatole <AmrutaPatole@users.noreply.github.com>
2020-05-08 18:45:00 +0530

    Code file created.

---
+++ Data_analysis/code.py
@@ -0,0 +1,116 @@
+# --------------
+# Importing header files
+import numpy as np
+ 
+path
+# Path of the file has been stored in variable called 'path'
+
+#New record
+new_record=np.array([[50,  9,  4,  1,  0,  0, 40,  0]])
+data=np.genfromtxt(path, delimiter=",", skip_header=1)
+
+census=np.concatenate((data,new_record), axis = 0)
+#Code starts here
+
+
+
+# --------------
+#Code starts here
+import numpy as np
+path
+new_record=np.array([[50,  9,  4,  1,  0,  0, 40,  0]])
+data=np.genfromtxt(path, delimiter=",", skip_header=1)
+
+#add new record row-wise to data array
+census=np.concatenate((data,new_record), axis = 0)
+age=np.array(census[:,0])
+max_age=age.max()
+min_age=age.min()
+age_mean=age.mean()
+age_std=np.std(age)
+
+
+
+# --------------
+#Code starts here
+import numpy as np
+ 
+path
+# Path of the file has been stored in variable called 'path'
+
+#New record
+new_record=np.array([[50,  9,  4,  1,  0,  0, 40,  0]])
+data=np.genfromtxt(path, delimiter=",", skip_header=1)
+
+census=np.concatenate((data,new_record), axis = 0)
+
+#extract race coloumn
+
+
+race_0=census[census[:,2]==0]
+race_1=census[census[:,2]==1]
+race_2=census[census[:,2]==2]
+race_3=census[census[:,2]==3]
+race_4=census[census[:,2]==4]
+print(race_3)
+#calulate length of each variable
+len_0=len(race_0)
+len_1=len(race_1)
+len_2=len(race_2)
+len_3=len(race_3)
+len_4=len(race_4)
+print(len_3)
+a=[len_0,len_1,len_2,len_3,len_4]
+minority_race = np.argmin(a)
+print(minority_race)
+#print(minority_race)
+#print(a)
+
+
+
+
+# --------------
+import numpy as np
+ 
+path
+# Path of the file has been stored in variable called 'path'
+
+#New record
+new_record=np.array([[50,  9,  4,  1,  0,  0, 40,  0]])
+data=np.genfromtxt(path, delimiter=",", skip_header=1)
+
+census=np.concatenate((data,new_record), axis = 0)
+
+senior_citizens=census[census[:,0]>60]
+senior_citizens_len=len(senior_citizens)
+
+working_hours_sum=senior_citizens.sum(axis=0)[6]
+avg_working_hours=working_hours_sum/senior_citizens_len
+print(working_hours_sum)
+
+
+# --------------
+#Code starts here
+import numpy as np
+ 
+path
+# Path of the file has been stored in variable called 'path'
+
+#New record
+new_record=np.array([[50,  9,  4,  1,  0,  0, 40,  0]])
+data=np.genfromtxt(path, delimiter=",", skip_header=1)
+
+census=np.concatenate((data,new_record), axis = 0)
+
+high=census[census[:,1]>10]
+low=census[census[:,1]<=10]
+
+avg_pay_high=high.mean(axis=0)[7]
+avg_pay_low=low.mean(axis=0)[7]
+print('The statment better education leads to better pay is ',avg_pay_high > avg_pay_low)
+
+
+
+
+
+
